5f8272278ae62afbe6de23d723c677baa2536808
Michael MacCallum <micksmaccallum@gmail.com>
2016-09-21 18:40:49 -0400

    added user, network and encoder tables

---
+++ sql/create_tables.sql
@@ -0,0 +1,25 @@
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  is_admin BOOLEAN NOT NULL DEFAULT(0),
+  handle TEXT NOT NULL,
+  hashed_password TEXT NOT NULL,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS network;
+CREATE TABLE network (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS encoder;
+CREATE TABLE encoder (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  ip_address TEXT NOT NULL,
+  port INTEGER NOT NULL,
+  network_id INTEGER NOT NULL,
+  FOREIGN KEY(network_id) REFERENCES network(id)
+);
