d7c9b8251a69ada51d3b72b35b7fe9eb866b3985
Michael MacCallum <micksmaccallum@gmail.com>
2016-09-22 18:04:22 -0400

    removing drops for now

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,5 +1,5 @@
 
-DROP TABLE IF EXISTS user;
+/*DROP TABLE IF EXISTS user;*/
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   is_admin BOOLEAN NOT NULL DEFAULT(0),
@@ -10,13 +10,13 @@ CREATE TABLE user (
   status INTEGER NOT NULL DEFAULT(0)
 );
 
-DROP TABLE IF EXISTS network;
+/*DROP TABLE IF EXISTS network;*/
 CREATE TABLE network (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL
 );
 
-DROP TABLE IF EXISTS encoder;
+/*DROP TABLE IF EXISTS encoder;*/
 CREATE TABLE encoder (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   ip_address TEXT NOT NULL,
@@ -29,14 +29,12 @@ CREATE TABLE encoder (
 
 DROP TABLE IF EXISTS connection;
 CREATE TABLE connection (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id INTEGER PRIMARY KEY AUTOINCREMENT
 
-  captioner_id INTEGER NOT NULL,
-  FOREIGN KEY(captioner_id) REFERENCES user(id)
-
+  /*captioner_id INTEGER NOT NULL,
+  FOREIGN KEY(captioner_id) REFERENCES user(id),
   network_id INTEGER NOT NULL,
-  FOREIGN KEY(network_id) REFERENCES network(id)
-
-  encoder_id INTEGER NOT NULL,
-  FOREIGN KEY(encoder_id) REFERENCES encoder(id)
+  FOREIGN KEY(network_id) REFERENCES network(id),*/
+  /*encoder_id INTEGER NOT NULL,
+  FOREIGN KEY(encoder_id) REFERENCES encoder(id)*/
 );
