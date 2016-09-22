92d5dc7fccd40b1700e9a8b90ff810392b248c1f
Michael MacCallum <micksmaccallum@gmail.com>
2016-09-21 23:01:07 -0400

    added connection table and status columns to network and user

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -6,7 +6,8 @@ CREATE TABLE user (
   handle TEXT NOT NULL,
   hashed_password TEXT NOT NULL,
   first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL
+  last_name TEXT NOT NULL,
+  status INTEGER NOT NULL DEFAULT(0)
 );
 
 DROP TABLE IF EXISTS network;
@@ -20,6 +21,22 @@ CREATE TABLE encoder (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   ip_address TEXT NOT NULL,
   port INTEGER NOT NULL,
+  status INTEGER NOT NULL DEFAULT(0),
+
   network_id INTEGER NOT NULL,
   FOREIGN KEY(network_id) REFERENCES network(id)
+);
+
+DROP TABLE IF EXISTS connection;
+CREATE TABLE connection (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+  captioner_id INTEGER NOT NULL,
+  FOREIGN KEY(captioner_id) REFERENCES user(id)
+
+  network_id INTEGER NOT NULL,
+  FOREIGN KEY(network_id) REFERENCES network(id)
+
+  encoder_id INTEGER NOT NULL,
+  FOREIGN KEY(encoder_id) REFERENCES encoder(id)
 );
