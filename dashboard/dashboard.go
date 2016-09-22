c1edb6d874f0ce2680be7dadede5b4cc2a2907de
Michael MacCallum <micksmaccallum@gmail.com>
2016-09-22 18:05:24 -0400

    DB connection in dashboard package

--- dashboard/dashboard.go
+++ dashboard/dashboard.go
@@ -1,9 +1,21 @@
 package dashboard
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+
+	// Passing lint
+	_ "github.com/mattn/go-sqlite3"
 )
+
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("sqlite3", "database.db")
+	checkErr(err)
+}
 
 // Start starts the HTTP server
 func Start() {
@@ -13,5 +25,11 @@ func Start() {
 		panic(err)
 	} else {
 		log.Println("Server running on :8080")
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
