1788d246eb7b8ab3b53b31161df167b4df990753
Michael MacCallum <micksmaccallum@gmail.com>
2016-09-26 01:19:21 -0400

    removed commented code

--- dashboard/router.go
+++ dashboard/router.go
@@ -22,16 +22,6 @@ func serveStaticFolder(folder string) {
 	http.Handle(folder, http.StripPrefix(folder, http.FileServer(http.Dir(static))))
 }
 
-// func serveRoute(route string, templateName string) {
-// 	http.HandleFunc(route, func(writer http.ResponseWriter, _request *http.Request) {
-// 		template := templateOnBase(fmt.Sprintf("templates/%s", templateName))
-//
-// 		if err := template.Execute(writer, nil); err != nil {
-// 			http.Error(writer, err.Error(), http.StatusInternalServerError)
-// 		}
-// 	})
-// }
-
 func handleNetworks() {
 	http.HandleFunc("/network.html", func(writer http.ResponseWriter, request *http.Request) {
 
