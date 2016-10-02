e6f4892fc8198d6e872ed22bf1c8b4056a3b5704
Michael MacCallum <micksmaccallum@gmail.com>
2016-10-02 16:27:46 -0400

    fixed add encoder

--- persist/networks.go
+++ persist/networks.go
@@ -1,15 +1,63 @@
 package persist
 
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+)
+
 // AddEncoder adds a new encoder for the given network
-func AddEncoder(encoder Encoder, network Network) error {
+func AddEncoder(encoder Encoder, network Network) (*Encoder, error) {
 	query := `
     INSERT INTO encoder (
-      ip_address, port, status, network_id
+      name, ip_address, port, status, network_id
     ) VALUES (
-      ?, ?, ?, ?
+      ?, ?, ?, ?, ?
     );
   `
+	result, err := db.Exec(query, encoder.Name, encoder.IPAddress, encoder.Port, encoder.Status, network.ID)
+	if err != nil {
+		return nil, err
+	}
 
-	_, err := db.Exec(query, encoder.IPAddress, encoder.Port, encoder.Status, network.ID)
-	return err
+	rowID, err := result.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	newEncoder := Encoder{
+		ID:        sql.NullInt64{Int64: rowID, Valid: true},
+		Name:      encoder.Name,
+		IPAddress: encoder.IPAddress,
+		Port:      encoder.Port,
+		Status:    encoder.Status,
+		NetworkID: network.ID,
+	}
+
+	return &newEncoder, nil
+}
+
+// EncoderToJSON Removes SQL fields and transforms to a []byte of JSON data.
+func EncoderToJSON(encoder Encoder) ([]byte, error) {
+	if !encoder.ID.Valid || !encoder.Name.Valid {
+		return nil, errors.New("Error validating Encoder")
+	}
+
+	newEncoder := struct {
+		ID        int64
+		Name      string
+		IPAddress string
+		Port      int
+		Status    int
+		NetworkID int
+	}{
+		encoder.ID.Int64,
+		encoder.Name.String,
+		encoder.IPAddress,
+		encoder.Port,
+		encoder.Status,
+		encoder.NetworkID,
+	}
+
+	return json.Marshal(newEncoder)
 }
