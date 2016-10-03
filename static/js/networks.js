c3f52092b3cdb549f8ff5fd92fe360746fbeb2fa
Michael MacCallum <micksmaccallum@gmail.com>
2016-10-02 22:58:29 -0400

    increment row numbers to 1 index instead of 0

--- static/js/networks.js
+++ static/js/networks.js
@@ -3,14 +3,17 @@ addEncoder = function(encoder) {
     return false;
   }
 
+  var body = $('#encoder-selection-table > tbody');
+  var count = body.children().length;
+
   var row = $('<tr></tr>');
-  row.append('<th scope=row></th>');
+  row.append('<th scope=row>' + (count + 1) + '</th>');
   row.append('<td>' + encoder.IPAddress + '</td>');
   row.append('<td>' + encoder.Port + '</td>');
   row.append('<td>' + encoder.Name + '</td>');
   row.append('<td>' + encoder.Status + '</td>');
 
-  $('#encoder-selection-table > tbody').append(row);
+  body.append(row);
   return true;
 };
 
