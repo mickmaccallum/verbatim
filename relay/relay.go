273fd8ac066a6c2ff0315b0b52160a00d511f801
Andrew Owen <yumaikas94@gmail.com>
2016-10-09 01:15:43 -0400

    Giving 0x7fs some stuff

--- relay/relay.go
+++ relay/relay.go
@@ -2,13 +2,11 @@ package relay
 
 import (
 	"fmt"
-	"github.com/0x7fffffff/verbatim/persist"
 	"log"
 	"net"
 	_ "os"
-	"strconv"
-	"strings"
 	"sync"
+	"github.com/0x7fffffff/verbatim/persist"
 )
 
 // The type of a given activity
@@ -34,10 +32,8 @@ var connMux = &sync.RWMutex{}
 
 var encoderIdToConn = make(map[int]net.Conn)
 
-// TODO
-// Connect this to a captioners list?
-func notifyCaptionerConnected() {
-
+func NotifyPortAdded(portNum int, n persist.Network) {
+	log.Println("Port added", portNum, n.Name)
 }
 
 //
@@ -76,85 +72,6 @@ func AddDownstreamConnection(encoder persist.Encoder) error {
 
 	log.Panic("NOT DONE YET")
 	return nil
-}
-
-// Listen for TCP connections
-func Start() error {
-	ln, err := net.Listen("tcp", "localhost:6000")
-	log.Println("Lisenting on port 6000")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		go handleConnection(conn)
-	}
-}
-
-// This is now sort of working. We'll need to use this to work out how to send data around
-func authenticateUser(c net.Conn) (*persist.Network, error) {
-	buf := make([]byte, 128)
-	_, err := c.Write([]byte("Enter the id of the network you want to connect to:"))
-	if err != nil {
-		c.Close()
-		return nil, err
-	}
-
-	// Will read block?
-	n, err := c.Read(buf)
-	log.Println("Buff", string(buf[0:n]))
-	if err != nil {
-		c.Close()
-	}
-
-	// #runningwithscissors
-	id, err := strconv.Atoi(strings.Trim(string(buf[0:n]), "\r\n\t "))
-	if err != nil {
-		fmt.Fprintln(c, "Invalid network id, please reconnect and try again")
-		log.Println(err.Error())
-		c.Close()
-		return nil, err
-	}
-
-	network, err := persist.GetNetwork(id)
-	if err != nil {
-		fmt.Fprintln(c, "Unable to find network!")
-		c.Close()
-		return nil, err
-	}
-	return network, nil
-}
-
-// Basic demoable state.
-func handleConnection(c net.Conn) {
-	// TODO: Authenticate a new user
-	network, err := authenticateUser(c)
-	if err != nil {
-		// Bail
-		return
-	}
-
-	fmt.Fprintln(c, "Connected to echo server for:", network.Name)
-
-	buf := make([]byte, 4096)
-
-	for {
-		n, err := c.Read(buf)
-		if err != nil || n == 0 {
-			c.Close()
-			break
-		}
-		n, err = c.Write(buf[0:n])
-		if err != nil {
-			c.Close()
-			break
-		}
-	}
-	log.Printf("Connection from %v closed.", c.RemoteAddr())
 }
 
 /*
