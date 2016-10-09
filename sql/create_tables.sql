cd971804362fc3b9af0ace85b675f0aa47ae7b7d
Michael MacCallum <micksmaccallum@gmail.com>
2016-10-09 02:48:26 -0400

    commented out drops

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,19 +1,19 @@
 
-drop table if exists user;
+/*drop table if exists user;*/
 create table if not exists admin (
   id int primary key,
   handle text not null,
   hashed_password text not null
 );
 
-drop table if exists network;
+/*drop table if exists network;*/
 create table if not exists network (
   id integer primary key,
   listening_port int unique not null,
   name text not null
 );
 
-drop table if exists encoder;
+/*drop table if exists encoder;*/
 create table if not exists encoder (
   id int primary key,
   ip_address text not null,
