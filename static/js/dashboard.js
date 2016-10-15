c0a06a2797bd11764e90f2a2c8359c3cd2234906
Michael MacCallum <micksmaccallum@gmail.com>
2016-10-15 03:43:13 -0400

    refactoring and started adding websocket js

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,4 +1,4 @@
-$(function () {
+function addNetworkListListeners() {
   $('#network-selection-table > tbody > tr').click(function(e) {
     var id = $(e.currentTarget).attr('data-network-id');
 
@@ -10,4 +10,46 @@ $(function () {
 
     return false;
   });
+}
+
+var ws;
+
+function startWebSocket() {
+  if (ws != null) {
+    return;
+  }
+
+  ws = new WebSocket(socketURL);
+
+  ws.onopen = function(event) {
+    console.log("OPEN");
+
+    console.log("sending message");
+
+    var payload = {
+      "message": "Hello, Servar."
+    };
+
+    ws.send(JSON.stringify(payload));
+  }
+
+  ws.onclose = function(event) {
+    console.log("CLOSE");
+    ws = null;
+  }
+
+  ws.onmessage = function(event) {
+    var msg = JSON.parse(event.data);
+
+    console.log("RESPONSE: " + JSON.stringify(msg));
+  }
+
+  ws.onerror = function(event) {
+    console.log("ERROR: " + event.data);
+  }
+}
+
+$(function () {
+  addNetworkListListeners();
+  startWebSocket();
 });
