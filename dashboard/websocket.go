9854840d71303648e73058c06f31875cff526a50
Michael MacCallum <micksmaccallum@gmail.com>
2016-10-15 03:44:04 -0400

    added web socket go file

---
+++ dashboard/websocket.go
@@ -0,0 +1,41 @@
+package dashboard
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func startSocket(router *mux.Router) {
+	log.Println("starting socket")
+	router.HandleFunc("/socket", openSocket)
+}
+
+func openSocket(writer http.ResponseWriter, request *http.Request) {
+	conn, err := upgrader.Upgrade(writer, request, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer conn.Close()
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		log.Printf("recv: %s", message)
+
+		err = conn.WriteMessage(messageType, message)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+	}
+}
