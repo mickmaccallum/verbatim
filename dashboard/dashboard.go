f0f9e1109b8e03de756a970bf20f4c48dcc61c48
Michael MacCallum <micksmaccallum@gmail.com>
2016-10-16 22:19:20 -0400

    We'll come back to this later. Sick of getting 403s

--- dashboard/dashboard.go
+++ dashboard/dashboard.go
@@ -6,7 +6,7 @@ import (
 	// Passing lint
 	_ "github.com/0x7fffffff/verbatim/persist"
 	"github.com/0x7fffffff/verbatim/websocket"
-	"github.com/gorilla/csrf"
+	// "github.com/gorilla/csrf"
 	"github.com/gorilla/mux"
 )
 
@@ -18,9 +18,9 @@ func Start() {
 
 	// Switch these lines for production
 	// protected := csrf.Protect([]byte("tb82Tg0Hw8vVQ6cO8TP1Yh9D69M0lKX4"))(router)
-	protected := csrf.Protect([]byte("tb82Tg0Hw8vVQ6cO8TP1Yh9D69M0lKX4"), csrf.Secure(false))(router)
+	// protected := csrf.Protect([]byte("tb82Tg0Hw8vVQ6cO8TP1Yh9D69M0lKX4"), csrf.Secure(false))(router)
 
-	if err := http.ListenAndServe("127.0.0.1:4000", protected); err != nil {
+	if err := http.ListenAndServe("127.0.0.1:4000", nil /*protected*/); err != nil {
 		panic(err)
 	}
 }
