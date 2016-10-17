eee400078464379c75129fdcadcd38e8fa419fe6
Goomplez <jfzachariah3593@eagle.fgcu.edu>
2016-10-16 21:08:49 -0400

    added network submission

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -28,6 +28,41 @@ $(function () {
   }).catch(function(event) {
     console.log(event);
   });
+
+  $('#submit-network').click(function (e) {
+    var port = $('#network-form-port').val().trim();
+    var name = $('#network-form-name').val().trim();
+
+    var data = {
+      'port': port,
+      'name': name
+    }
+
+    $.ajax({
+      url: '/network/add',
+      type: 'POST',
+      dataType: 'json',
+      data: data,
+      success: function(network) {
+        if (addNetwork(network)) {
+
+        } else {
+
+        }
+
+        $('#network-form-port').val('');
+        $('#network-form-name').val('');
+      },
+      error: function (xhr, ajaxOptions, thrownError) {
+        console.log('++++++++++++++++++++++++++++++++');
+        console.log(xhr);
+        console.log(ajaxOptions);
+        console.log(thrownError);
+        console.log('--------------------------------');
+      }
+    });
+  });
+
 });
 
 // var payload = {
