d1cdc882ea71c276f561c5ef42282f607596bc96
Michael MacCallum <micksmaccallum@gmail.com>
2016-10-22 10:13:25 -0400

    started work on bulk message send

--- websocket/websocket.go
+++ websocket/websocket.go
@@ -3,17 +3,67 @@ package websocket
 import (
 	"log"
 	"net/http"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
+	// "sync/atomic"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{} // use default options
+var messages = make(chan SocketMessage, 10)
+
+// TODO: Figure out how to remove conns on disconnect.
+var sockets = make(map[*websocket.Conn]struct{})
+var mutex = &sync.Mutex{}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		components := strings.Split(r.RemoteAddr, ":")
+		return components[0] == "127.0.0.1"
+	},
+}
 
 // Start starts the socket
 func Start(router *mux.Router) {
-	log.Println("starting socket")
 	router.HandleFunc("/socket", openSocket)
+
+	go spin()
+
+	time.AfterFunc(10*time.Second, func() {
+		payload := make(map[string]string)
+		payload["message"] = "Hello, browser"
+
+		m := &SocketMessage{
+			Payload: payload,
+		}
+		log.Println("Sending Message")
+		m.Send()
+	})
+}
+
+func spin() {
+	for {
+		select {
+		case message := <-messages:
+			for conn := range sockets {
+				wrapper := struct {
+					Payload interface{}
+				}{
+					message.Payload,
+				}
+
+				err := conn.WriteJSON(wrapper)
+				if err != nil {
+					log.Println(err)
+				}
+			}
+		}
+
+		runtime.Gosched()
+	}
 }
 
 func openSocket(writer http.ResponseWriter, request *http.Request) {
@@ -23,20 +73,26 @@ func openSocket(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	defer conn.Close()
-	for {
-		messageType, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			break
-		}
+	mutex.Lock()
+	sockets[conn] = struct{}{}
+	mutex.Unlock()
 
-		log.Printf("recv: %s", message)
+	log.Println(sockets)
 
-		err = conn.WriteMessage(messageType, message)
-		if err != nil {
-			log.Println(err)
-			break
-		}
-	}
+	// defer conn.Close()
+	// for {
+	// 	messageType, message, err := conn.ReadMessage()
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 		break
+	// 	}
+
+	// 	log.Printf("recv: %s", message)
+
+	// 	err = conn.WriteMessage(messageType, message)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 		break
+	// 	}
+	// }
 }
