b2b7de85037b305e1ece0d4ff0b532d1484ca6c9
Michael MacCallum <micksmaccallum@gmail.com>
2016-10-23 13:54:31 -0400

    track references in SocketMessage

--- websocket/message.go
+++ websocket/message.go
@@ -2,7 +2,8 @@ package websocket
 
 // SocketMessage A message to be emitted over all open websockets
 type SocketMessage struct {
-	Payload interface{}
+	Reference *int
+	Payload   interface{}
 }
 
 // Send emits the receiver as a message across all websocket connections.
