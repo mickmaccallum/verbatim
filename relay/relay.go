e015d15360f8ac4b2857099c1beb566e803fd3ad
Andrew Owen <yumaikas94@gmail.com>
2016-10-28 03:29:42 -0400

    Now name is accurate

--- relay/relay.go
+++ relay/relay.go
@@ -10,46 +10,6 @@ import (
 	"github.com/0x7fffffff/verbatim/states"
 )
 
-// TODO: maintain a source of truth state that can be handed to web pages
-
-type encoderListener struct{}
-
-func (e encoderListener) NetworkListenFailed(network model.Network) {
-	dashboard.NetworkPortStateChanged(network, states.NetworkListenFailed)
-}
-
-func (e encoderListener) NetworkListenSucceeded(network model.Network) {
-	dashboard.NetworkPortStateChanged(network, states.NetworkListening)
-}
-
-func (e encoderListener) NetworkRemoved(network model.NetworkID) {
-	// FIXME: Need to figure out a better way for this.
-	var netModel = model.Network{
-		ID: network,
-	}
-	dashboard.NetworkPortStateChanged(netModel, states.NetworkClosed)
-}
-
-func (e encoderListener) Connected(ci model.CaptionerID) {
-	dashboard.CaptionerStateChanged(ci, states.CaptionerConnected)
-}
-
-func (e encoderListener) Disconnected(ci model.CaptionerID) {
-	dashboard.CaptionerStateChanged(ci, states.CaptionerMuted)
-}
-
-func (e encoderListener) Muted(ci model.CaptionerID) {
-	dashboard.CaptionerStateChanged(ci, states.CaptionerMuted)
-}
-
-func (e encoderListener) Unmuted(ci model.CaptionerID) {
-	dashboard.CaptionerStateChanged(ci, states.CaptionerUnmuted)
-}
-
-func (e encoderListener) GetBroadcaster(network model.Network) *megaphone.NetworkBroadcaster {
-	return megaphone.GetBroadcasterForNetwork(network.ID)
-}
-
 // ActivityType The type of a given activity
 type ActivityType int
 
@@ -61,7 +21,7 @@ func NotifyPortAdded(portNum int, n model.Network) {
 // Start I'm a stub.
 func Start() {
 	go dashboard.Start(nil)
-	go microphone.Start(encoderListener{})
+	go microphone.Start(micListener{})
 	go megaphone.Start(nil)
 
 }
