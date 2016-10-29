3cb224f4f00f7bf26f163d9f15c7826ea97b29e1
Michael MacCallum <micksmaccallum@gmail.com>
2016-10-29 14:09:56 -0400

    tmp

--- dashboard/dashboard.go
+++ dashboard/dashboard.go
@@ -63,8 +63,11 @@ type RelayListener interface {
 var relay RelayListener
 
 // Start starts the HTTP server
-func Start(l RelayListener) {
-	relay = l
+func Start(l *RelayListener) {
+	// TODO: Remove pointer here.
+	if l != nil {
+		relay = *l
+	}
 	// store.Codecs = securecookie.CodecsFromPairs(securecookie.GenerateRandomKey(32))
 	store.Options = &sessions.Options{
 		Path:     "/",
