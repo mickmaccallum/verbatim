30f8d998dced69ee58e92f7b77dec3a9928c1cb4
Michael MacCallum <micksmaccallum@gmail.com>
2016-11-13 13:01:12 -0500

    added basics for updating password

--- static/js/account.js
+++ static/js/account.js
@@ -14,11 +14,44 @@ function addHandleChangeListener() {
   });
 };
 
+function validatePasswords(password, confirm) {
+  if (password !== confirm) {
+    return false;
+  }
+
+  return password.length >= 8 && password.length <= 255
+};
+
 function addPasswordChangeListener() {
-	
+  $('#submit-password-change').click(function(event) {
+    var passwordField = $('#admin-form-password');
+    var confirmPasswordField = $('#admin-form-confirm-password');
+
+    if (!validatePasswords(passwordField.val(), confirmPasswordField.val())) {
+      console.log('password isn\'t valid.');
+      return;
+    }
+
+    $.ajax({
+      url: '/account/password',
+      type: 'POST',
+      data: $('#admin-password-form').serialize(),
+    }).done(function(response) {
+      passwordField.val('');
+      confirmPasswordField.val('');
+    }).fail(function() {
+      console.log("error");
+      console.log(this);
+    });
+  });	
+};
+
+function addDeleteAdminListener() {
+  
 };
 
 $(function () {
   addHandleChangeListener();
   addPasswordChangeListener();
+  addDeleteAdminListener();
 });
