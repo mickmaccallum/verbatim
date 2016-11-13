063ff3c4d8c24cd2d1eccf13e1a6c12bb58a25ff
Michael MacCallum <micksmaccallum@gmail.com>
2016-11-13 13:00:35 -0500

    formatting

--- static/js/account.js
+++ static/js/account.js
@@ -1,17 +1,10 @@
 function addHandleChangeListener() {
   $('#submit-handle-change').click(function(event) {
-
-    var data = $('#admin-handle-form').serialize();
-    console.log($('#admin-handle-form'));
-    console.log(data);
-
     $.ajax({
       url: '/account/handle',
       type: 'POST',
-      dataType: 'json',
-      // contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-      data: data,
-    }).done(function(network) {
+      data: $('#admin-handle-form').serialize(),
+    }).done(function(response) {
       // $('#network-form-port').val('');
       // $('#network-form-name').val('');
     }).fail(function() {
