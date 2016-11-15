656a75caa41ba14fd9f02ea1f1178156dc8fd0c8
Michael MacCallum <micksmaccallum@gmail.com>
2016-11-14 22:16:08 -0500

    fixed login redirect for various GETs

--- dashboard/router.go
+++ dashboard/router.go
@@ -125,7 +125,7 @@ func handleAccountsPage(router *mux.Router) {
 	router.HandleFunc("/account", func(writer http.ResponseWriter, request *http.Request) {
 		_, sessionOk := checkSessionValidity(request)
 		if !sessionOk {
-			writer.WriteHeader(http.StatusUnauthorized)
+			redirectLogin(writer, request)
 			return
 		}
 
@@ -320,6 +320,12 @@ func handleLogin(router *mux.Router) {
 	}).Methods("GET")
 
 	router.HandleFunc("/login", func(writer http.ResponseWriter, request *http.Request) {
+		// session, sessionOk := checkSessionValidity(request)
+		// if !sessionOk {
+		// 	writer.WriteHeader(http.StatusUnauthorized)
+		// 	return
+		// }
+
 		if err := request.ParseForm(); err != nil {
 			clientError(writer, err)
 			return
@@ -378,7 +384,7 @@ func handleCaptionersPage(router *mux.Router) {
 	router.HandleFunc("/captioners", func(writer http.ResponseWriter, request *http.Request) {
 		_, sessionOk := checkSessionValidity(request)
 		if !sessionOk {
-			writer.WriteHeader(http.StatusUnauthorized)
+			redirectLogin(writer, request)
 			return
 		}
 
@@ -532,11 +538,15 @@ func handleNetworksPage(router *mux.Router) {
 
 	// Get Encoder
 	router.HandleFunc("/networks/{network_id:[0-9]+}", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("Trying to get network")
+
 		_, sessionOk := checkSessionValidity(request)
 		if !sessionOk {
-			writer.WriteHeader(http.StatusUnauthorized)
+			redirectLogin(writer, request)
 			return
 		}
+
+		log.Println("Got valid session")
 
 		id := identifierFromRequest("network_id", request)
 		if id == nil {
@@ -600,7 +610,7 @@ func handleDashboardPage(router *mux.Router) {
 	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		_, sessionOk := checkSessionValidity(request)
 		if !sessionOk {
-			writer.WriteHeader(http.StatusUnauthorized)
+			redirectLogin(writer, request)
 			return
 		}
 
