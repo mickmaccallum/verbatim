e033c5e0b38a06eddbaa8f0b5b3950417756ece1
Michael MacCallum <micksmaccallum@gmail.com>
2016-11-22 15:00:37 -0500

    added error prompting function

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,9 @@
 $(function() {
   $('#logout-button').click(function(event) {
-  	$(this).closest('form').submit();
+    $(this).closest('form').submit();
   });
 });
+
+function alertError(error) {
+  alert('Error: ' + error);
+};
