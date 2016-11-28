7fa0c63cbe0fced9b6cc84f0e788ff8f5c1454a7
Michael MacCallum <micksmaccallum@gmail.com>
2016-11-27 20:31:16 -0500

    uber derp

--- static/js/networks.js
+++ static/js/networks.js
@@ -8,6 +8,7 @@ $(function() {
   autoStopWebSocket();
   addMuteCaptionerListners();
   addUnmuteCaptionerListners();
+  addDisconnectCaptionerListeners();
 });
 
 function changeEncoderState(encoder, encoderState) {
